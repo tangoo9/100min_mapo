13030bd7905475d019f839a38ae91e164106d7ab
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2021-11-24 09:01:33 +0900

    211123 반응형 레이아웃 (모바일, 25/33)

--- MapoCommunityCare/js/app-user-intro.js
+++ MapoCommunityCare/js/app-user-intro.js
@@ -1,7 +1,15 @@
 
 
 // app-user-join-accept.html
-const check = document.querySelector("check");
+// const check = document.querySelector("check");
 
 
 //app-user-intro.js
+
+
+// app-user-info-detail boot
+const change = document.querySelector('.selectButton');
+const changeP = document.querySelector('.app-user-info-detail-change');
+change.onclick = () => {
+    changeP.style.display ='block';
+}
