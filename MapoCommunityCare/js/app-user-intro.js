374cb1a36d239251283b72b371043a94a324a817
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2021-11-26 16:12:19 +0900

    21.11.26 mcc 1차 작업

--- MapoCommunityCare/js/app-user-intro.js
+++ MapoCommunityCare/js/app-user-intro.js
@@ -1,14 +1,12 @@
+//app-user-intro.js
 
 setTimeout(function(){
     location.href="app-user-intro.html"
 },5000);
 
 
-// app-user-join-accept.html
-// const check = document.querySelector("check");
 
 
-//app-user-intro.js
 
 
 
