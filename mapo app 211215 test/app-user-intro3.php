5153b37d49adddb12c5e4867b5b86c85b7567551
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2021-12-20 09:02:00 +0900

    21.12.17 MCC 아이돌봄신청하기 4/4 업뎃

---
+++ mapo app 211215 test/app-user-intro3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>마포커뮤니티케어</title>
+    <link rel="stylesheet" href="css/static-style.css">
+    <link rel="stylesheet" href="css/app-user-intro.css">
+</head>
+<body>
+        <main class="introPage_1">
+            <h1 class="title">인트로3</h1>
+            <p>
+                이용자 관련 수집할 개인정보 내용 추가 부분 ⟶
+            </p>
+            <p>
+                이용자 성별추가(필수)/아동의 경우 학교/학년(선택)
+            </p>
+
+            <button class="nextButton" type="button" onclick="location.href='app-user-intro4'">
+                >
+            </button>
+</body>
+</html>
