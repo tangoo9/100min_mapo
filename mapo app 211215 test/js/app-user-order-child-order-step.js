7bccfac4cfe69feb76dd963997fed2c92aa912d4
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2021-12-29 17:55:21 +0900

    21.12.29 MCC 메인페이지 복구, 박스 선택시 밸류 추가하기

--- mapo app 211215 test/js/app-user-order-child-order-step.js
+++ mapo app 211215 test/js/app-user-order-child-order-step.js
@@ -3,7 +3,7 @@
     
     // 색상 변경
     let currentColor;
-    let changeColor = document.querySelectorAll('.care_selectBtn');
+    let careSelect = document.querySelectorAll('.care_selectBtn');
 
     function colorHandler(){
     if (currentColor){
@@ -11,12 +11,30 @@
     }   
         this.classList.add('on');
         currentColor = this;
+
     }
-    for (let i = 0; i < changeColor.length; i++){   
-        changeColor[i].addEventListener('click', colorHandler);        
+    for (let i = 0; i < careSelect.length; i++){   
+        careSelect[i].addEventListener('click', colorHandler);        
     }
 
 
+    function select(temp){
+        document.getElementById('care_type').value = temp;
+        console.log(temp)
+    }
+    // function select(temp){
+    //     document.getElementById('care_type').value = temp;
+    //     console.log(temp)
+    // }
+
+
+
+
+
+
+    
+
+    //전체동의
     function selectAll(selectAll)  {
         const checkboxes = document.getElementsByName('agree');
         
@@ -24,8 +42,6 @@
             checkbox.checked = selectAll.checked;
         })
     }
-    
-
     // let obj = document.querySelector('.care_select_cont')
     // let slb = document.querySelector('.add_child')   
     // let delete_icon = document.querySelector('.delete')
@@ -37,3 +53,23 @@
     // }
 
 
+
+    //버전 1 아이디로 가져오고 하나하나 밸류값을 추가하는 방법?
+    // let input_value = document.getElementById('val_periodcare');
+    // let input_value2 = document.getElementById('val_timecare');
+
+    // function colorHandler(){
+    //     if (currentColor){
+    //         currentColor.classList.remove('on');
+    //     }   
+    //         this.classList.add('on');
+    //         currentColor = this;
+    //         console.log(input_value)
+    //         input_value.setAttribute("value", "selected");
+    
+    // }
+
+    // for (let i = 0; i < changeColor.length; i++){   
+    //     changeColor[i].addEventListener('click', colorHandler);  
+    // }
+    
--- mapo app 211215 test/js/formobile.js
+++ mapo app 211215 test/js/formobile.js
@@ -1,16 +1,38 @@
 const input = document.querySelector('input')
 
-input.addEventListener('keypress', function(event){
-    let key = event.key || event.keyCode;
-    
-    if (key === 'Enter' || key === 13) {
-        // key =  console.log('전송');
-        event.preventDefault();
-        input.blur();
-    }
-})
 
-document.querySelector('input')
+history.pushState(null, null, location.href); 
+
+window.onpopstate = function(event) { 
+
+    alert("여기에 작성하고 싶은 코드를 작성하면 됩니다!");
+    // history.back()
+
+}
+
+
+var checkPageShow = false;
+window.onpageshow = function(event){
+    checkPageShow = true;
+    if(event.persisted || 
+        (window.performance && window.performance.navigation.type==2)){
+        history.back();
+    }
+}
+
+
+
+// input.addEventListener('keypress', function(event){
+//     let key = event.key || event.keyCode;
+    
+//     if (key === 'Enter' || key === 13) {
+//         // key =  console.log('전송');
+//         event.preventDefault();
+//         input.blur();
+//     }
+// })
+l 
+// input.onfocus =this.blur()
 
 // function submitTextarea(event) {
 
@@ -19,26 +41,5 @@ document.querySelector('input')
 // let textarea = document.getElementById('my-textarea');
 // textarea.addEventListener('keyup', event => submitTextarea(event));
 
-/*
-모바일에서 datepicker사용중 가상키보드와 겹쳐서 픽커사용이 힘들고 스크립트 실행에 방해요소 있어 찾아보니 다음이 가장 쉬운 적용인거 같습니다.
 
-
-
-1. onfocus="this.blur()" 적용
-
- <input type="text" onfocus="this.blur()" ~~
-
-
-
-2. html5에서 도입된 inputmode = "none"적용
-
- <input type="text" ~~ inputmode="none">
-
-
-
-
-
-
-
-참고로 inputmode에는 none, text, decimal, numeric, tel, search, email, url 등의 값을 줄수 있고 값에 따른 적절한 가상키보드가 노출되는데 브라우저별로 적용, 미적용이 있습니다.
-*/
+// inputmode에는 none, text, decimal, numeric, tel, search, email, url
