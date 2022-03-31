2a2325e14d33507ae509a99e1e249660ef1c7551
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2022-03-31 12:18:48 +0900

    220331 돌봄이음 관련 외 자료 이동

--- MapoCommunityCare/js/app-user-join-accept.js
+++
@@ -1,7 +0,0 @@
-function selectAll(selectAll)  {
-    const checkboxes = document.getElementsByName('agree');
-    
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = selectAll.checked;
-    })
-  }
